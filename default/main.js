db33cdbabad4f80fe9bc05803d627aa03b042d55
cluelesswalnut <ernst.sukram@gmail.com>
2018-07-21 19:24:36 -0400

    added auto spawn upgrader

--- default/main.js
+++ default/main.js
@@ -12,18 +12,30 @@ module.exports.loop = function () {
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    console.log('Upgraders: ' + upgraders.length);
 
-    if(harvesters.length < 2) {
+    var minHarvesters = 2;
+    var minUpgraders = 2;
+
+    if(harvesters.length < minHarvesters) {
         var newName = 'Harvester' + Game.time;
         console.log('Spawning new harvester: ' + newName);
         Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
             {memory: {role: 'harvester'}});
     }
 
+    if(upgraders.length < minUpgraders) {
+        var newName = 'upgraders' + Game.time;
+        console.log('Spawning new upgraders: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+            {memory: {role: 'upgrader'}});
+    }
+
     if(Game.spawns['Spawn1'].spawning) {
         var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
         Game.spawns['Spawn1'].room.visual.text(
-            'ðŸ› ï¸' + spawningCreep.memory.role,
+            'spawning: ' + spawningCreep.memory.role,
             Game.spawns['Spawn1'].pos.x + 1,
             Game.spawns['Spawn1'].pos.y,
             {align: 'left', opacity: 0.8});
